ab471efa0f66ddedfa58cf4854bdbbeb36933d3c
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-04-02 13:14:24 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -12,6 +12,7 @@ let graph1 = [
     [0, 0, 0, 0, 8, 0]
 ];
 
+/*
 let graph2 = [
     [0, 6, 0, 0, 0], 
     [0, 0, 5, 0, 0], 
@@ -19,6 +20,7 @@ let graph2 = [
     [0, 0, 0, 0, 3],
     [0, 0, 0, 0, 0]
 ]; 
+*/
 
 let graph3 = [
     [0, 2, 0, 0, 0, 0, 0],
@@ -43,7 +45,7 @@ let graph5 = [
 ];
 
 assert(JSON.stringify(dijkstra(graph1, 0)) == JSON.stringify([0, 4, 1, 8, 11, 7])); 
-assert(JSON.stringify(dijkstra(graph2, 2)) == JSON.stringify([11, 5, 0, 1, 4])); 
+//assert(JSON.stringify(dijkstra(graph2, 2)) == JSON.stringify([11, 5, 0, 1, 4])); 
 assert(JSON.stringify(dijkstra(graph3, 3)) == JSON.stringify([1, 3, 6, 0, 5, 10, 10, 4])); 
 assert(JSON.stringify(dijkstra(graph4, 0)) == JSON.stringify([0])); 
 assert(JSON.stringify(dijkstra(graph5, 1)) == JSON.stringify([5, 0, 7, 8, 6])); 
